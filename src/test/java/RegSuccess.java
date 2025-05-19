9dc4474b1fd5450dbb37021d529e426577e6aa80
ylvimaksimova2022-group <ylvimaksimova2022@gmail.com>
2025-05-19 17:26:02 +0300

    Final commit

--- src/test/java/RegSuccess.java
+++ src/test/java/RegSuccess.java
@@ -1,7 +1,7 @@
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class ReggitSuccess {
+public class RegSuccess {
     private static Integer id;
 
     public static Integer getId() {
